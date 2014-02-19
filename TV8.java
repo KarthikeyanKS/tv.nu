b0ca3051c41e5906c52b696426e8532dccc76b4c
KarthikeyanKS <k.s.karthikeyan@gmail.com>
2014-02-19 14:55:36 +0530

    Create TV8.java
    Due to time constraint I did only for TV8 by getting xpath. It is possible to validate for all the channels in a single go by getting entire li webelements in List and iterating through it...

---
+++ TV8.java
@@ -0,0 +1,42 @@
+package tv.nu;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TV8 {
+  @Test
+  public void validateFutureBroadcast() {
+  
+	  WebDriver driver = new FirefoxDriver();
+	  driver.get("http://tv.nu");
+   
+	  List<WebElement> liElements = driver.findElements(By.xpath(".//*[@id='page_left']/div[4]/div[2]/div[5]/div[2]/div[2]/ul/li"));
+	  
+	  //System.out.println("Size of liElements  --  "+liElements.size());
+  	
+  	  int futurebroadcast = 0;
+  	
+  	  for(WebElement li : liElements){
+  		  String temp1 = li.getAttribute("class");
+//  		  String temp = li.getText();
+//  		  System.out.println("Text in li ---  "+temp);
+//  		  System.out.println("class in li ---  "+temp1);
+  		  if(temp1.equals("py")){
+  			  futurebroadcast = futurebroadcast+1;
+  		  }
+  	  }
+  	  if(futurebroadcast == 0){
+  		  System.out.println("Warning... There is nothing scheduled for future broadcast in TV8");
+  	  }
+  	  
+//  	  else System.out.println("total future broadcasts in queue for TV8 -- "+futurebroadcast);
+  	  
+	  driver.quit();
+// Due to time constraint I did only for TV8 by getting xpath. It is possible to validate for all the channels in a single go by getting entire li webelements in List and iterating through it...
+  }
+}
